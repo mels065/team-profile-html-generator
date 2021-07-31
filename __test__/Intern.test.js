d4a978f2559312a3ff08c559d4d7d40c0192ddad
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-31 19:21:57 -0400

    Alter Intern test to have more consistent structure

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -1,16 +1,18 @@
 const Intern = require('../lib/Intern');
 
 describe('Intern', () => {
-    it('should create an instance of Intern', () => {
-        const result = new Intern(
-            'Dave',
-            '12345',
-            'dave@gmail.com',
-            'School of Rock'
-        );
-        expect(result).toHaveProperty('name');
-        expect(result).toHaveProperty('id');
-        expect(result).toHaveProperty('email');
-        expect(result).toHaveProperty('school');
-    })
+    describe('initialization', () => {
+        it('should create an instance of Intern', () => {
+            const result = new Intern(
+                'Dave',
+                '12345',
+                'dave@gmail.com',
+                'School of Rock'
+            );
+            expect(result).toHaveProperty('name');
+            expect(result).toHaveProperty('id');
+            expect(result).toHaveProperty('email');
+            expect(result).toHaveProperty('school');
+        });
+    });
 });
