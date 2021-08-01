6ffee4d3ec04dd1a6086b18dda85fd9dc0124754
Brandon Mellus <mellusbrandon@gmail.com>
2021-07-31 22:40:46 -0400

    Create function for rendering renderEmployee

--- src/render/index.js
+++ src/render/index.js
@@ -2,7 +2,26 @@ const renderEmployee = require("./render-employee");
 
 function renderData(data) {
     const { manager, engineers, interns } = data;
-    return renderEmployee(manager);
+    return `
+    <html>
+        <head>
+            <title>Team Profile</title>
+            <link href="./style.css" rel="stylesheet" type="text/css" />
+        </head>
+        <body>
+            <header>
+                <h1>My Team</h1>
+            </header>
+            <main>
+                <div id="team">
+                    ${renderEmployee(manager)}
+                    ${engineers.map(engineer => renderEmployee(engineer)).join('\n')}
+                    ${interns.map(intern => renderEmployee(intern)).join('\n')}
+                </div>
+            </main>
+        </body>
+    </html>
+    `
 }
 
 module.exports = renderData;
